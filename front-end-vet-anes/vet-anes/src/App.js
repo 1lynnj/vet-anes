2cca76fb2f0c723417e7e8ea937284904964e08c
Lynn <lynn@jansheski.com>
2023-01-23 14:41:22 -0800

    Successfully Connected Front End to Back End
    *installed axios
    *created test axios call in front end to get drug list
    *created Test Component to test front/back connection
    *updated back end with CORS functionality

--- front-end-vet-anes/vet-anes/src/App.js
+++ front-end-vet-anes/vet-anes/src/App.js
@@ -1,18 +1,38 @@
 import "./App.css";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import Test from "./components/testComponent";
 
 function App() {
-  axios
-    .get()
-    .then((response) => {
-      console.log(`${JSON.stringify(response)}`);
-      // DO STUFF
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const [drugList, setDrugList] = useState([]);
 
-  return <h1>Hello!</h1>;
+  const loadDrugList = () => {
+    axios
+      .get("http://127.0.0.1:8000/protocol/drugs")
+      .then((response) => {
+        const updatedDrugList = response.data.map((drug) => {
+          return {
+            id: drug.id,
+            name: drug.name,
+            concentration: drug.concentration,
+            concentration_units: drug.concentration_units,
+            route: drug.route,
+          };
+        });
+        setDrugList(updatedDrugList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(loadDrugList, []);
+
+  return (
+    <div>
+      <Test drugList={drugList} loadDrugList={loadDrugList}></Test>
+    </div>
+  );
 }
 
 export default App;
--- vet_anes/vet_anes/settings.py
+++ vet_anes/vet_anes/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'protocol.apps.ProtocolConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'vet_anes.urls'
