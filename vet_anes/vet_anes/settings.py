30b4aba5a668f52e3809950609c7a8951fb9b6de
Lynn <lynn@jansheski.com>
2023-01-24 12:01:03 -0800

    Added cors origins to settings.py

--- vet_anes/vet_anes/settings.py
+++ vet_anes/vet_anes/settings.py
@@ -61,7 +61,10 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://vet-anes-front.herokuapp.com'
+    'https://vet-anes-front.herokuapp.com',
+    'https://vet-anes.herokuapp.com', 
+    'http://127.0.0.1:8000', 
+    'http://127.0.0.1'
 ]
 
 ROOT_URLCONF = 'vet_anes.urls'
