18786839d1a2bdb4d732438fb0c33be383317399
Lynn <lynn@jansheski.com>
2023-01-31 07:28:48 -0800

    increased character limit on drug name in Drug model

--- vet_anes/protocol/models.py
+++ vet_anes/protocol/models.py
@@ -3,7 +3,7 @@ from vet_anes.models import BaseModel
 
 # Create your models here.
 class Drug(BaseModel):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     concentration = models.FloatField(blank=True)
     concentration_units = models.CharField(max_length=10)
     rxcui_code = models.IntegerField()
