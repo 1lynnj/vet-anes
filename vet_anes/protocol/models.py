93f978f6e2f7c53a93956cdbcfdebefce71a7b04
Lynn <lynn@jansheski.com>
2023-02-07 16:42:32 -0800

    Updated drug route to allow longer string

--- vet_anes/protocol/models.py
+++ vet_anes/protocol/models.py
@@ -7,7 +7,7 @@ class Drug(BaseModel):
     concentration = models.FloatField(blank=True)
     concentration_units = models.CharField(max_length=10)
     rxcui_code = models.IntegerField()
-    route = models.CharField(max_length=10)
+    route = models.CharField(max_length=50)
     er_dose = models.FloatField(default=0)
 
     def __str__(self):
